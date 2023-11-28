df69df2ad28619e8034d44fed59d1892008d4baa
aditya136 <adityabanerjee102@gmail.com>
2023-11-29 00:38:23 +0600

    str,primitive-types-&-reference-type,object-in-js

--- script.js
+++ script.js
@@ -16,3 +16,75 @@ console.log( typeof booli)
 let info = "Aditya Banerjee"
 console.log(info.length)
 
+
+
+
+
+
+
+//variable
+// Cannot be a reserved Keyword
+// should be meaningful
+// Cannot start with a number (1name)
+// Cannot contain a space or hyphen (-)
+// Are case-sensitive
+
+//  Upper Case(A,B, C) & Lower Case(a, b, c)
+
+
+// let nameofProgrammer = "Virus"
+
+
+// console.log(nameofProgrammer)
+
+// const rollNumber = 9
+// // rollNumber = 10
+
+// console.log(rollNumber)
+
+
+
+// Primitive types
+
+// Boolean
+// String
+// Number
+// undefined
+// null
+// Symbol
+
+let nameOfStudent = "Aninda"
+let isPresent = false
+let rollNumber = 9
+let isAttentive = null
+
+// null -> memory allocated
+// undefined -> memory not allocated
+
+// Reference types
+// Object
+// Array
+// function
+
+
+// Object
+// key value pair
+let student = {
+    studentName: "Aninda",
+    isPresent: true,
+    rollNumber: 15
+}
+
+// Property access
+// Dot notation
+console.log(student.isPresent)
+
+// Bracket notation
+let selectedProperty = "rollNumber"
+console.log(student[selectedProperty])
+
+// Human
+// property
+// 2 hands, 2 legs, 1 head, 2 eye
+// method
+// walk(), run(), eat(), talk()
